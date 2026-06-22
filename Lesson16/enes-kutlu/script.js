b877e7608496308bce4d7c657094ed4cce2d9425
Enes <eneskutllu@icloud.com>
2026-06-22 18:14:11 +0300

    Update Lesson16/enes-kutlu/script.js
    Co-authored-by: Code2Career <info@annaatalay.com>

--- Lesson16/enes-kutlu/script.js
+++ Lesson16/enes-kutlu/script.js
@@ -268,7 +268,7 @@ function countOccurrences(arr, word) {
   }
   let count = 0;
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] == word) {
+    if (arr[i] === word) {
       count++;
     }
   }
