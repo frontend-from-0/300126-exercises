bf2ae75413bd40c6a613b2e062b52d9c6492d133
tugbacode-7 <tugbaclki@outlook.com>
2026-08-26 16:19:02 +0300

    Lesson24-tugba-kutlu

---
+++ Lesson24/Lesson24/tugba-kutlu/pages/update.js
@@ -0,0 +1,56 @@
+const searchParams = new URLSearchParams(window.location.search);
+
+const userId = searchParams.get("userId");
+
+const updateUserForm = document.getElementById("updateUserForm");
+const statusMessage = document.getElementById("statusMessage");
+const firstNameInput = document.getElementById("firstName");
+
+if (!userId) {
+  updateUserForm.classList.add("hidden");
+  statusMessage.textContent = "The user is not found.";
+  statusMessage.classList.remove("hidden");
+}
+
+fetch(`https://dummyjson.com/users/${userId}`)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`Failed to fetch user with id ${userId}`);
+    }
+    return response.json();
+  })
+  .then((user) => {
+    firstNameInput.value = user.firstName;
+  })
+  .catch((error) => {
+    statusMessage.textContent = "An error occured." + error;
+    statusMessage.classList.remove("hidden");
+  });
+
+updateUserForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  statusMessage.classList.add("hidden");
+
+  fetch(`https://dummyjson.com/users/${userId}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      firstName: firstNameInput.value,
+    }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Failed to update user ${userId}`);
+      }
+      return response.json();
+    })
+    .then((updatedUser) => {
+      statusMessage.textContent = `User updated successfully. New name: ${updatedUser.firstName}`;
+      statusMessage.classList.remove("hidden");
+    })
+    .catch((error) => {
+      statusMessage.textContent = "An error occured." + error;
+      statusMessage.classList.remove("hidden");
+    });
+});
